3141a6c12f353bea9c9ea995e7f8b9fc920a9fca
sfjbssept <sfjbssept@gmail.com>
2022-09-21 09:49:57 +0000

    json response added

--- node-js/index.js
+++ node-js/index.js
@@ -3,7 +3,21 @@
 const http = require('http');
 
 function greet(req, res) {
-    res.write("<h1>Hi this is Joy, hope you have great day.</h1>");
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(
+        JSON.stringify({
+            name: "Prabodh",
+            empId: "001",
+            address: {
+                street: "XYZ Street",
+                city: "Pune",
+                state: "Maharashtra"
+            }
+        })
+    )
+
+    // res.write("<h1>Hi this is Joy, hope you have great day.</h1>");
+    res.end();
 }
 
 http.createServer(greet).listen(5000);
