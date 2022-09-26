9a2c092efe721410b119ce84a549eff74d6ae26c
sfjbssept <sfjbssept@gmail.com>
2022-09-26 14:37:46 +0000

    Update and delete operation

--- node-js/node-rest-api-mongo/routes/courses.js
+++ node-js/node-rest-api-mongo/routes/courses.js
@@ -6,6 +6,7 @@ const Course = require('../models/course');
 // get all courses
 router.get('/allcourses', async (req, res) => {
     try {
+        console.log(req);
         const courses = await Course.find();
         res.json(courses);
     } catch (err) {
@@ -16,13 +17,35 @@ router.get('/allcourses', async (req, res) => {
 // add a course
 router.post('/addcourse', async (req, res) => {
     try {
-        console.log(req.body);
-        const Course = await Course.create(req.body);
-        res.json(Course);
+        const course = await Course.create(req.body);
+        res.json(course);
     } catch (err) {
         res.json(err)
     }
 })
 
+// delete course
+router.delete('/delete/:courseId', async (req, res) => {
+    try {
+        await Course.remove({ _id: req.params.courseId });
+        res.status(200).json({
+            message: 'deleted successfully'
+        });
+    } catch (err) {
+        res.json(err)
+    }
+});
+
+// update course
+router.put('/update/:courseId', async (req, res) => {
+
+    const courseId = req.params.courseId;
+    try {
+        const course = await Course.updateOne({ _id: courseId }, req.body);
+        res.json(course)
+    } catch (err) {
+        res.json(err)
+    }
+});
 
 module.exports = router;
