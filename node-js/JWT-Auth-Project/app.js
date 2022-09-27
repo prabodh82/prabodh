fdf676c0746f121dc731b5891d6f5d7f0e98d6d4
sfjbssept <sfjbssept@gmail.com>
2022-09-27 14:43:34 +0000

    create user changes added

--- node-js/JWT-Auth-Project/app.js
+++ node-js/JWT-Auth-Project/app.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const userRouter = require("./api/users/user.router");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/api', (req, res) => {
-    res.json({
-        sucess: 1,
-        message: 'This rest api is working'
-    })
-})
+app.use(express.json());
+app.use("/api/users", userRouter);
+
+// app.get('/api', (req, res) => {
+//     res.json({
+//         sucess: 1,
+//         message: 'This rest api is working'
+//     })
+// })
 
 app.listen(PORT, () => {
     console.log(`server is up and running on ${PORT}`);
