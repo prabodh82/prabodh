f3a8a294a11ab4bb7909512d4ae1168661fc53bc
sfjbssept <sfjbssept@gmail.com>
2022-09-29 14:55:14 +0000

    token verification done

--- node-js/JWT-Auth-Project/api/users/user.router.js
+++ node-js/JWT-Auth-Project/api/users/user.router.js
@@ -1,11 +1,13 @@
-const { createUser, getUsers, getUserById, updateUser, deleteUser, login } = require("./user.controller");
 const router = require("express").Router();
+const { createUser, getUsers, getUserById, updateUser, deleteUser, login } = require("./user.controller");
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.patch("/:id", updateUser);
-router.delete("/:id", deleteUser);
+const { checkToken } = require("../../auth/token_validation");
+
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUserById);
+router.patch("/:id", checkToken, updateUser);
+router.delete("/:id", checkToken, deleteUser);
 router.post("/login", login)
 
 
