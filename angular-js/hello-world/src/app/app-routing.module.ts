96c12653ae7a905c0f0bd57bf81705c49aa44dd6
sfjbssept <sfjbssept@gmail.com>
2022-11-24 10:56:23 +0000

    created cusom pipe

--- angular-js/hello-world/src/app/app-routing.module.ts
+++ angular-js/hello-world/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { PipeDemoComponent } from './components/pipe-demo/pipe-demo.component';
 import { RegisteredUsersComponent } from './components/registered-users/registered-users.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent }, 
   { path: "register", component: UserFormComponent },
-  { path: "registered", component: RegisteredUsersComponent }
+  { path: "registered", component: RegisteredUsersComponent },
+  { path :"pipe", component: PipeDemoComponent}
 ];
 
 @NgModule({
--- angular-js/hello-world/src/app/app.module.ts
+++ angular-js/hello-world/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisteredUsersComponent } from './components/registered-users/registered-users.component';
+import { PipeDemoComponent } from './components/pipe-demo/pipe-demo.component';
+import { EllipsisPipe } from './Pipes/ellipsis.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { RegisteredUsersComponent } from './components/registered-users/register
     NavbarComponent,
     UserFormComponent,
     HomeComponent,
-    RegisteredUsersComponent
+    RegisteredUsersComponent,
+    PipeDemoComponent,
+    EllipsisPipe
   ],
   imports: [
     BrowserModule,
